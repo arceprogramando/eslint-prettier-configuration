f6f08824693e8cd3336ac4a26dd71182482bda46
Felipe Arce <85712228+arceprogramando@users.noreply.github.com>
2024-07-05 00:54:16 -0300

    feat(config):create a screenshot sh of sss_cli

---
+++ captura.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sss --area "0,0 1920x1080" --background "gradient.png" --shadow --shadow-color "#07031280" --shadow-blur 30 --radius 10 --author "Felipe Arce" --author-color "#ffffff" --author-font "=14.0" -o ~/Downloads/nuevo.png
